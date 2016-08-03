b304fb155af8e59207d54cd811fb904f0c830e5f
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-08-03 16:58:25 +0400

    simplify interface

--- src/App.js
+++ src/App.js
@@ -6,33 +6,34 @@ import convertRoute from './convertRoute'
 import './App.css'
 
 class App extends Component {
+  url = (train) => `https://rasp.yandex.ru/threads/?number=${train}&train=yes`
   state = {}
+  onSubmit = (e) => {
+    window.open(this.url(this.state.trainNumber), '', 'width=500,height=700,left=400,top=100')
+    e.preventDefault()
+  }
+  trainNumberChange = (e) => {
+    this.setState({ trainNumber: e.target.value })
+  }
   onPaste = (e) => {
     this.setState({ route: convertRoute(extractRoute(e.clipboardData.getData('text/html'))) })
+    e.preventDefault()
   }
   render() {
     return (
-      <div className="App">
+      <div className="App" onPaste={this.onPaste}>
         <p className="App-intro">
-          На этой странице можно узнать когда будет работать мобильный интернет в поезде.
+          На этой странице можно узнать в какое время будет работать мобильный интернет в вашей поездке по железной дороге.
         </p>
-        <Step number={1}>
-          <p className="App-intro">
-            Выберите станции отправления, прибытия и дату.
-          </p>
-          <Widget/>
-        </Step>
-        <Step number={2}>
-          <p className="App-intro">
-            Выберите рейс и перейдите на страницу маршрута.
-          </p>
-        </Step>
-        <Step number={3}>
-          <p className="App-intro">
-            Нажмите ctrl+A, ctrl+C чтобы скопировать страницу маршрута целиком и вставьте ее в это поле:
-          </p>
-          <textarea onPaste={this.onPaste} style={{width: 700, height: 50}}></textarea>
-        </Step>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.trainNumberChange} placeholder="Номер поезда"/>
+          <input type="submit" value="Найти расписание"/>
+        </form>
+        <p className="App-intro">
+          График движения с Яндекс расписаний откроется в новом окне, скопируйте открывшуюся страницу целиком, нажав ctrl+A, ctrl+C.
+          Переключитесь обратно на эту страницу и вставьте содержимое в поле расположенное ниже.
+        </p>
+        <textarea onPaste={this.onPaste} style={{width: 700, height: 50}}></textarea>
         <div>
           {JSON.stringify(this.state.route)}
         </div>
